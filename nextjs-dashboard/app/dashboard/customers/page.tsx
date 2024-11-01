ca28a0eb328b49ad7ab30c94c684f83b45c6aa92
Cetin Erdem <cetoerdem1@gmail.com>
2024-11-01 17:58:27 +0400

    first commit

---
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -0,0 +1,13 @@
+// /dashboard/customers/page.js
+import React from "react";
+
+const CustomersPage = () => {
+  return (
+    <div>
+      <h1>Customers</h1>
+      {/* Other content */}
+    </div>
+  );
+};
+
+export default CustomersPage;
---
+++ nextjs-dashboard/app/dashboard/invoices/page.tsx
@@ -0,0 +1,13 @@
+// /dashboard/customers/page.js
+import React from "react";
+
+const InvoicesPage = () => {
+  return (
+    <div>
+      <h1>invoices</h1>
+      {/* Other content */}
+    </div>
+  );
+};
+
+export default InvoicesPage;
